1af71c30b82dbbdaad9168dea653a47c760fab85
KellanS <kunlinsongcode@gmail.com>
2024-02-13 19:03:15 +0800

    Test module done (Bootstrap) with annotation document.

---
+++ analysis_toolkit/_test.py
@@ -0,0 +1,131 @@
+from typing import Callable, NamedTuple, Optional, Union, overload
+
+import numpy as np
+from scipy import stats
+
+ConfidenceInterval = NamedTuple(
+    "ConfidenceInterval",
+    [("low", float), ("high", float)],
+)
+
+
+BootstrapTestResult = NamedTuple(
+    "BootstrapTestResult",
+    [
+        ("statistic", float),
+        ("distribution", np.ndarray),
+        ("confidence_interval", ConfidenceInterval),
+    ],
+)
+
+
+BootstrapTestResults = NamedTuple(
+    "BootstrapTestResultList",
+    [
+        ("statistic", float),
+        ("distribution", np.ndarray),
+        ("confidence_interval", dict[float, ConfidenceInterval]),
+    ],
+)
+
+
+class BootstrapTest:
+    """
+    A class that performs bootstrap hypothesis testing.
+
+    Args:
+        statistic (Callable): The statistic to be computed on resampled
+        data. Default is np.mean.
+        n_resamples (int): The number of resamples to generate. Default
+        is 10000.
+
+    Returns:
+        BootstrapTestResult or BootstrapTestResults: The result of the
+        bootstrap test.
+
+    Examples:
+        >>> test = BootstrapTest()
+        >>> result = test(data, confidence_level=0.95)
+    """
+
+    RANDOM_SEED = 42
+    CONFIDENCE_METHOD = "BCa"
+
+    def __init__(self, statistic: Callable = np.mean, n_resamples: int = 10000) -> None:
+        self.statistic = statistic
+        self.n_resamples = n_resamples
+
+    @overload
+    def __call__(
+        self, data: np.ndarray, *, confidence_level: float
+    ) -> BootstrapTestResult: ...
+
+    @overload
+    def __call__(
+        self, data: np.ndarray, *, confidence_levels: list[float]
+    ) -> BootstrapTestResults: ...
+
+    def __call__(
+        self,
+        data: np.ndarray,
+        *,
+        confidence_level: Optional[float] = None,
+        confidence_levels: Optional[list[float]] = None
+    ) -> Union[BootstrapTestResult, BootstrapTestResults]:
+        """
+        Perform bootstrap hypothesis testing.
+
+        Args:
+            data (np.ndarray): The input data.
+            confidence_level (float, optional): The confidence level for
+            the test. Default is None.
+            confidence_levels (list[float], optional): The list of
+            confidence levels for the test. Default is None.
+
+        Returns:
+            BootstrapTestResult or BootstrapTestResults: The result of
+            the bootstrap test.
+        """
+        if confidence_level is not None:
+            res = stats.bootstrap(
+                data=data,
+                statistic=self.statistic,
+                n_resamples=self.n_resamples,
+                confidence_level=confidence_level,
+                method=self.CONFIDENCE_METHOD,
+                random_state=self.RANDOM_SEED,
+            )
+            statistic = np.median(res.bootstrap_distribution)
+            distribution = res.bootstrap_distribution
+            confidence_interval = ConfidenceInterval(
+                low=res.confidence_interval.low, high=res.confidence_interval.high
+            )
+            return BootstrapTestResult(
+                statistic=statistic,
+                distribution=distribution,
+                confidence_interval=confidence_interval,
+            )
+        elif confidence_levels is not None:
+            confidence_intervals = {}
+            statistic = None
+            for confidence_level in confidence_levels:
+                res = stats.bootstrap(
+                    data=data,
+                    statistic=self.statistic,
+                    n_resamples=self.n_resamples,
+                    confidence_level=confidence_level,
+                    method=self.CONFIDENCE_METHOD,
+                    random_state=self.RANDOM_SEED,
+                )
+                if statistic is None:
+                    statistic = np.median(res.bootstrap_distribution)
+                    distribution = res.bootstrap_distribution
+                confidence_interval = ConfidenceInterval(
+                    low=res.confidence_interval.low, high=res.confidence_interval.high
+                )
+                confidence_intervals[confidence_level] = confidence_interval
+            return BootstrapTestResults(
+                statistic=statistic,
+                distribution=distribution,
+                confidence_interval=confidence_intervals,
+            )
