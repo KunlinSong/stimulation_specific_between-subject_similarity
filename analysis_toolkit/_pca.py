d95d910bb8cc5501a45c5deab2fc2d6c422be91a
KellanS <kunlinsongcode@gmail.com>
2024-02-14 11:50:13 +0800

    Add PCA decomposition functions for 2D and 3D

--- analysis_toolkit/_pca.py
+++ analysis_toolkit/_pca.py
@@ -3,10 +3,12 @@ from sklearn.decomposition import PCA
 
 
 def pca_decomposition(
-    data_dict: dict[str, list[np.ndarray]]
+    data_dict: dict[str, list[np.ndarray]], n_components: int
 ) -> dict[str, list[np.ndarray]]:
     """
-    Perform PCA decomposition on the input data dictionary.
+    Perform PCA decomposition on the input data dictionary. The
+    decomposition is performed to reduce the data to the specified
+    number of components.
 
     Args:
         data_dict (dict[str, list[np.ndarray]]): A dictionary where the
@@ -18,7 +20,7 @@ def pca_decomposition(
         arrays.
 
     """
-    pca = PCA(n_components=2)
+    pca = PCA(n_components=n_components)
     v_lst = []
     for v in data_dict.values():
         v_lst.append(np.stack(v, axis=0))
@@ -30,3 +32,43 @@ def pca_decomposition(
         new_data = [pca.transform(x.reshape(1, -1)) for x in v]
         new_data_dict[k] = new_data
     return new_data_dict
+
+
+def pca_decomposition_2d(
+    data_dict: dict[str, list[np.ndarray]]
+) -> dict[str, list[np.ndarray]]:
+    """
+    Perform PCA decomposition on the input data dictionary. The
+    decomposition is performed to reduce the data to 2D.
+
+    Args:
+        data_dict (dict[str, list[np.ndarray]]): A dictionary where the
+        keys represent labels and the values are lists of numpy arrays.
+
+    Returns:
+        dict[str, list[np.ndarray]]: A dictionary where the keys
+        represent labels and the values are lists of transformed numpy
+        arrays.
+    """
+    n_components = 2
+    return pca_decomposition(data_dict, n_components)
+
+
+def pca_decomposition_3d(
+    data_dict: dict[str, list[np.ndarray]]
+) -> dict[str, list[np.ndarray]]:
+    """
+    Perform PCA decomposition on the input data dictionary. The
+    decomposition is performed to reduce the data to 3D.
+
+    Args:
+        data_dict (dict[str, list[np.ndarray]]): A dictionary where the
+        keys represent labels and the values are lists of numpy arrays.
+
+    Returns:
+        dict[str, list[np.ndarray]]: A dictionary where the keys
+        represent labels and the values are lists of transformed numpy
+        arrays.
+    """
+    n_components = 3
+    return pca_decomposition(data_dict, n_components)
